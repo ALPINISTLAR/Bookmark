0972ae3aa9cb177ce9c4986e52cc2d5bbf56669c
ALPINISTLAR <niveam424@gmail.com>
2023-04-02 12:52:13 +0200

    make feature tabs work

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const elsTabsItem = document.querySelectorAll('.tabs__item');
+const elsTabLink = document.querySelectorAll('.js-tab-link');
+const elsTabpanels = document.querySelectorAll('.tabpanels__item');
+
+
+function DeactivateTabItems () {
+  elsTabsItem.forEach(function (elTabsItem) {
+    elTabsItem.classList.remove('tabs__item--active');
+  });
+}
+
+function DeactivateTabpanels () {
+  elsTabpanels.forEach(function (elTabpanel) {
+    elTabpanel.classList.remove('tabpanels__item--active');
+  });
+}
+
+elsTabLink.forEach(function (elTabLink) {
+  elTabLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    DeactivateTabItems();
+    elTabLink.parentElement.classList.add('tabs__item--active');
+    DeactivateTabpanels();
+// const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
+const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
+elTargetPanel.classList.add('tabpanels__item--active');
+  });
+});
