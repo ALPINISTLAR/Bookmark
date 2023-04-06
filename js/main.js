3cd8fa35c34e982e27b8f9b14b69606f9471fa03
ALPINISTLAR <niveam424@gmail.com>
2023-04-06 09:44:09 +0200

    add FAQ section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const elsTabsItem = document.querySelectorAll('.tabs__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
 const elsTabpanels = document.querySelectorAll('.tabpanels__item');
+const elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
+const elsAccordionItem = document.querySelectorAll('.accordion__item');
 
 
 function DeactivateTabItems () {
@@ -15,6 +17,12 @@ function DeactivateTabpanels () {
   });
 }
 
+function CloseAccordionItems () {
+elsAccordionItem.forEach(function (elAccordionItem) {
+  elAccordionItem.classList.remove('accordion__item--open');
+});
+}
+
 elsTabLink.forEach(function (elTabLink) {
   elTabLink.addEventListener('click', function (evt) {
     evt.preventDefault();
@@ -24,5 +32,13 @@ elsTabLink.forEach(function (elTabLink) {
 // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
 const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
 elTargetPanel.classList.add('tabpanels__item--active');
+  });
+});
+
+
+elsAccordionItemToggler.forEach(function (elAccordionItemToggler) {
+  elAccordionItemToggler.addEventListener('click', function () {
+  CloseAccordionItems();
+elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
   });
 });
